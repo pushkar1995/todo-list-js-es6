01793c2ae06bb5fb78e2df71cb2ff8119e706d1d
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-07-20 21:32:06 +0545

    List Structure created with HMTL,CSS and array of todo tasks objects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import './index.scss';
+
+const tasks = [
+  {
+    description: 'Evening Tasks',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Afternoon Tasks',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Morning Tasks',
+    completed: false,
+    index: 0,
+  },
+];
+
+const viewTasks = () => {
+  const container = document.querySelector('.to-do-list');
+
+  // Sort tasks array based on index property
+  tasks.sort((a, b) => a.index - b.index);
+
+  tasks.forEach((task) => {
+    const listItem = document.createElement('li');
+    listItem.className = 'list-item';
+    listItem.innerHTML = `<input class="check-box" type="checkbox">${task.description}`;
+    container.appendChild(listItem);
+  });
+};
+window.addEventListener('load', viewTasks);
