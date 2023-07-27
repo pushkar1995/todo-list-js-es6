8e48d3d0b22d2a2071881b8b6c836a614e4f6c3c
Arch-Noize <santiagonoize@gmail.com>
2023-07-27 09:48:46 -0400

    Add testing function

--- src/modules/add.test.js
+++ src/modules/add.test.js
@@ -1,34 +1,45 @@
 /** @jest-environment jsdom */
 
 // import { addTask, removeTask } from "./AddTest.js";
-// import addNewTask from './modules/AddNewTask.js';
-// import removeTask from './modules/RemoveTask.js';
+import addTask from './modules/AddNewTask.js';
+import removeTask from './modules/RemoveTask.js';
 
-const addTask = require('./AddTest.js');
-const removeTask = require('./AddTest.js');
+// const addTask = require('./AddTest.js');
+// const removeTask = require('./AddTest.js');
 
 describe('Adding or removing items test', () => {
-  test('Adding items to the list', () => {
-    document.body.innerHTML = '<ul class="to-do-list"> </ul>';
-    const container = document.querySelector('.to-do-list');
-    const tasks = [];
+  document.body.innerHTML += '<ul class="to-do-list"> </ul>';
+  const container = document.querySelector('.to-do-list');
+  const tasks = [];
+  const listItem = document.createElement('li');
+  listItem.className = 'list-item';
+
+  test('Adding one item to the list', () => {
     tasks.push(addTask('Testing', tasks.length));
 
     expect(tasks).toEqual([{ description: 'Testing', completed: false, index: 0 }]);
-    // this one is the same as ===
+    
     tasks.forEach((task) => {
-      const listItem = document.createElement('li');
-      listItem.className = 'list-item';
       listItem.innerHTML = `
         <input class="check-box" type="checkbox" ${task.completed ? 'checked' : 'unchecked'}>
         <p class="description">${task.description}</p>
         `;
       container.appendChild(listItem);
     });
-    expect(container).toHaveLength(1);
+
+    const list = document.querySelectorAll('.list-item');
+
+    expect(list).toHaveLength(1);
   });
 
-  test('Removing items to the list', () => {
-    removeTask();
+  test('Removing one item from the list', () => {
+    // const list = document.querySelectorAll('.list-item');
+    // tasks.push(addTask('Testing', tasks.length));
+
+    removeTask(tasks, 0);
+
+    console.log(tasks);
+
+    // expect(tasks.length).toHaveLength(0);
   });
 });
