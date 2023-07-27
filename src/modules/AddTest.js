3da9aaf3c4ab226d342f7406114410b82eb45367
Arch-Noize <santiagonoize@gmail.com>
2023-07-27 03:33:05 -0400

    Add test files

---
+++ src/modules/AddTest.js
@@ -0,0 +1,18 @@
+export const addTask = (taskDescription, arrayLength) => {
+    const newTask = {
+      description: taskDescription,
+      completed: false,
+      index: arrayLength,
+    };
+  
+    return newTask;
+};
+
+export const removeTask = (tasks, index) => {
+    tasks.splice(index, 1);
+    let i = index;
+    while (i < tasks.length) {
+      tasks[i].index = i;
+      i += 1;
+    }
+  };
---
+++ src/modules/add.test.js
@@ -0,0 +1,3 @@
+/** @jest-environment jsdom */
+
+import { addTask, removeTask } from "./AddTest";
