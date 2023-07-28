0af5c6549f6ad54779ebea606a3df4c69a32428b
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-07-28 15:27:12 +0545

    Add Editing and Task Status Update test functionality

--- src/modules/add.test.js
+++ src/modules/add.test.js
@@ -31,13 +31,4 @@ describe('Adding or removing items test', () => {
 
     expect(tasks).toHaveLength(1);
   });
-
-  test('Removing one item from the list', () => {
-    tasks.push(addTask('Testing', tasks.length));
-    editTask(tasks, 0, 'Hi, Im new');
-
-    viewTask(tasks);
-
-    expect(tasks).toEqual([{ description: 'Hi, Im new', completed: false, index: 0 }]);
-  });
 });
--- src/modules/edit.test.js
+++ src/modules/edit.test.js
@@ -1,7 +1,32 @@
 /** @jest-environment jsdom */
 
-import { editTask, taskStatusUpdate, clearCompleted} from "./EditTest.js";
+import { editTask, taskStatusUpdate, clearCompleted } from './EditTest.js';
 
 describe('Testing editing and updating', () => {
-    
-})
+  let tasks;
+
+  beforeEach(() => {
+    tasks = [{
+      index: 1,
+      description: 'This is the test task',
+      completed: false,
+    }];
+  });
+
+  test('Editing one item of the list', () => {
+    editTask(tasks, 0, 'Testing');
+    expect(tasks).toEqual([{ description: 'Testing', completed: false, index: 1 }]);
+  });
+
+  test('Task Status Update of one item from the list', () => {
+    taskStatusUpdate(tasks, 0);
+    expect(tasks[0].completed).toEqual(true);
+  });
+
+  test('Clear all completed tasks from the list', () => {
+    tasks = tasks.push({});
+    taskStatusUpdate(tasks, 0);
+    clearCompleted(tasks);
+    console.log(tasks);
+  });
+});
