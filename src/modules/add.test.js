8205f0a37dbcfff2e67b7a93fbf0c027ecd3b00e
Arch-Noize <santiagonoize@gmail.com>
2023-07-28 04:38:03 -0400

    Add example function

--- src/modules/add.test.js
+++ src/modules/add.test.js
@@ -1,14 +1,14 @@
 /** @jest-environment jsdom */
 
-import {addTask , removeTask, viewTask} from "./AddTest.js";
+import { addTask, removeTask, viewTask } from './AddTest.js';
 
 describe('Adding or removing items test', () => {
   let tasks;
 
-  beforeEach (() => {
+  beforeEach(() => {
     tasks = [];
     document.body.innerHTML += '<ul class="to-do-list"> </ul>';
-  })
+  });
 
   test('Adding one item to the list', () => {
     tasks.push(addTask('Testing', tasks.length));
@@ -24,12 +24,20 @@ describe('Adding or removing items test', () => {
 
   test('Removing one item from the list', () => {
     tasks.push(addTask('Testing', tasks.length));
-
     tasks.push(addTask('Testing', tasks.length));
     removeTask(tasks, 0);
 
     viewTask(tasks);
 
     expect(tasks).toHaveLength(1);
+  });
+
+  test('Removing one item from the list', () => {
+    tasks.push(addTask('Testing', tasks.length));
+    editTask(tasks, 0, 'Hi, Im new');
+
+    viewTask(tasks);
+
+    expect(tasks).toEqual([{ description: 'Hi, Im new', completed: false, index: 0 }]);
   });
 });
